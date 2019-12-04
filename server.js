240d8877651c4a88ffee87616a5afe228720b18b
adeoye adela <adeoye.adela@gmail.com>
2019-12-04 19:25:46 +0100

    gatekeeper working

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
 const express = require('express'); // importing a CommonJS module
+const helmet= require ('helmet')
 
 const hubsRouter = require('./hubs/hubs-router.js');
 
 const server = express();
 
+//Custom middleware
+
+function gateKeeper( req, res, next){
+  const password = req.headers.password;
+  if (password && password.toLowerCase()){
+  next()
+  }
+  else{
+    res.status(401).json("You can't pass")
+  }
+}
+
 server.use(express.json());
+server.use(gateKeeper)
 
 server.use('/api/hubs', hubsRouter);
 
@@ -16,5 +30,8 @@ server.get('/', (req, res) => {
     <p>Welcome${nameInsert} to the Lambda Hubs API</p>
     `);
 });
+server.get("/area51",[ helmet(), gateKeeper], (req,res)=>{
 
+  res.send(req.headers)
+})
 module.exports = server;
